f8653ff7be44f9eb2648c9c2dfb7b1a071eb9a02
syuka <ayaka.katafuchi@rakus-partners.co.jp>
2025-01-23 11:48:19 +0900

    first-commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication { //起動用のmain
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
