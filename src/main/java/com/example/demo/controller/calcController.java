082e5dba4ad56272250ff69fb4bfe75b9b171f83
syuka <ayaka.katafuchi@rakus-partners.co.jp>
2025-01-23 17:34:03 +0900

    second-commit

---
+++ src/main/java/com/example/demo/controller/calcController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/calculation")
+
+public class calcController {
+
+    @RequestMapping("")
+    public String index(){
+        return "calcSum-form";
+    }
+
+    @RequestMapping("/input-num")
+    
+    public int calculation(String num1, String num2){
+        int result = Integer.parseInt(num1);
+        int result2 = Integer.parseInt(num2);
+        int answer = result + result2;
+
+        return "finished2";
+
+
+
+    }
+
+}
+
---
+++ src/main/java/com/example/demo/form/ReceiveForm.java
@@ -0,0 +1,22 @@
+package com.example.demo.form;
+
+public class ReceiveForm {
+
+    private String name;  //カプセル化したらゲッター・セッターする
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+    public String getAge() {
+        return age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+
+}
